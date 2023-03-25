68a0e552c60dca40eea1f2c1c1ab449ee74c44f1
jsalazarv <jsalazarv8@gmail.com>
2023-03-25 02:43:56 -0600

    Added gallery view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home/index.vue';
 import Shop from '@/views/Shop/index.vue';
+import Gallery from '@/views/Gallery/index.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/shop',
       name: 'shop',
       component: Shop,
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Gallery,
     },
     {
       path: '/about',
